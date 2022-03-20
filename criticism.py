ab218500e7807e5f6016d642c9b7081b9b5b13cf
jpb1991 <jpb6891@gmail.com>
2022-03-20 17:54:53 -0400

    Create criticism.py

---
+++ criticism.py
@@ -0,0 +1,10 @@
+def criticism():
+  print ("Stop it Alice the great!")
+  print ("Alice the great, you are a racist to little Bill the man child!")
+  print ("Alice the great, little Bill the man child is African American! So are you! You are a disgrace and a racist to your own race! The black police officer should lock you up in prison for life!")
+criticism()
+
+def criticism_who(who):
+  print (f"Hello {who} the ungreatful.")
+  print (f"I do not want to talk to you {who}.")
+criticism_who("Alice")
